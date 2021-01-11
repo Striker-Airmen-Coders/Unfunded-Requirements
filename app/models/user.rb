bab1fac768b55a735cac2bbde38ff8c3e2c05e06
Thomas Profitt <tom@rubyshore.com>
2021-01-11 16:28:08 -0700

    Add role to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable
+
+  VALID_ROLES = [nil, "admin"]
+  validates_inclusion_of :role, in: VALID_ROLES
 end
