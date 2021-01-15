18b8c17f31bb47d0010977e26c4cf008dd484166
Ariel Scott <nicolescott818@gmail.com>
2021-01-15 13:17:35 -0600

    Restrict regular users from accessing /admin and vice versa

---
+++ lib/user_authenticatable.rb
@@ -0,0 +1,31 @@
+require 'devise/strategies/authenticatable'
+
+module Devise
+  module Strategies
+    class UserAuthenticatable < Authenticatable
+      def authenticate!
+        controller = params["controller"]
+        email = params["user"]["email"]
+        password = params["user"]["password"]
+        user = User.find_for_authentication(:email => email)
+
+        if controller == "trestle/auth/sessions"
+          if user
+            if user.role != "admin"
+              fail!('Incorrect email or password')
+            end
+          end
+        end
+
+        if controller == "devise/sessions"
+          if user
+            if user.role == "admin"
+              fail!('Incorrect email or password')
+            end
+          end
+        end
+
+      end
+    end
+  end
+end
