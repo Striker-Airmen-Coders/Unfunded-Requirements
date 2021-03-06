573d8d4c59c461fdbb9786221a7d88efe8c78c92
moldux.dartus <moldux.dartus@protonmail.com>
2021-03-06 09:29:55 -0600

    emptied

--- app/controllers/example_pages_controller.rb
+++
@@ -1,5 +0,0 @@
-class ExamplePagesController < ApplicationController
-  def one_dialog
-    @page_title = "Example Page: one_dialog"
-  end
-end
--- app/jobs/example_job.rb
+++
@@ -1,9 +0,0 @@
-class ExampleJob < ApplicationJob
-  queue_as :default
-
-  def perform(*args)
-    # Just prints the current time to a tempfile.
-    # Useful for confirming Jobs are working
-    File.write("/tmp/#{self.class.name}_last_run", DateTime.now.to_s)
-  end
-end
