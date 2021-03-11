a949eb2c251ea65d92e0a683b31c0bfbaa1944df
moldux.dartus <moldux.dartus@protonmail.com>
2021-03-11 10:17:50 -0600

    edit page partly themed

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'pages/fma', to: "pages#fma", as: "fma"
   get 'pages/unit', to: "pages#unit", as: "unit"
   get 'pages/edit', to: "pages#edit", as: "edit"
-  root to: 'pages#type'
+  root to: 'reqs#index'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
