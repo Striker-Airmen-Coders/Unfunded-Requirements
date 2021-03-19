d3cb0f3257b34045f02a4ac2e02a5f6ff33f35df
moldux.dartus <moldux.dartus@protonmail.com>
2021-03-19 03:53:53 -0500

    this is halfway working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
+global.toastr = require("toastr")
 import "stylesheets/application"
 
 Rails.start()
