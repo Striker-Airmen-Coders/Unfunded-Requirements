e9f9f580f7fd55ce1322cf50d55a162c533c6899
Nicholas Phillips <nicholas.d.phillips@gmail.com>
2021-03-30 11:02:38 -0500

    Remove Toastr and fix Font Awesome (use js SVGs).

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "@fortawesome/fontawesome-free/css/all"
+import "@fortawesome/fontawesome-free/js/all"
 
 import "channels"
 import "controllers"
