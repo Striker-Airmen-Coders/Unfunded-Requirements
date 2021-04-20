903ad5777778b63fe7ec54acec448a5e7264ea02
moldux.dartus <moldux.dartus@protonmail.com>
2021-04-20 04:01:25 -0500

    migrations now fully cleaned, dbr fields commented out

--- db/migrate/20210301215658_create_reqs.rb
+++ db/migrate/20210301215658_create_reqs.rb
@@ -29,6 +29,36 @@ class CreateReqs < ActiveRecord::Migration[6.1]
       t.string :installation, default: 'Barksdale'   
 
       t.timestamps
+
+      #these fields are here as an example of what DBR would need
+#      t.boolean :is_recurring
+#      t.string :final_fy
+#      t.boolean :is_civ_pay
+#      t.string :contract_number
+#      t.date :pop_start_date
+#      t.date :pop_end_date
+#      t.string :created_by_org
+#      t.string :reviewed_by
+#      t.string :object_class
+#      t.string :esp
+#      t.string :aai
+#      t.decimal :requirement_total
+#      t.decimal :wing_funded_total
+#      t.decimal :unfunded_total_by_wing
+#      t.text :pem_remarks
+#      t.text :hq_fma_remarks
+#      t.decimal :ep_funded_amount
+#      t.decimal :unfunded_amount_by_ep
+#      t.date :need_by_date
+#      t.string :ba
+#      t.string :sag
+#      t.string :panel 
+#      t.string :priority_level
+#      t.integer :owner_ranking
+#      t.string :requirement_number
+#      t.string :status
+
+
     end
   end
 end
