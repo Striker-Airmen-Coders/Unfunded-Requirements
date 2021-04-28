a6d1de079bd4ec4250578b1dae15bd7b9e2febb3
moldux.dartus <moldux.dartus@protonmail.com>
2021-04-28 14:12:29 -0500

    removed extra users from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,4 @@ require './db/seeds/office_seed.rb'
 require './db/seeds/question_seed.rb'
 require './db/seeds/user_seed.rb'
 #require './db/seeds/req_seed.rb'
+# This can be uncommented unless it's necessary to demonstrate bulk upload
--- db/seeds/user_seed.rb
+++ db/seeds/user_seed.rb
@@ -1,50 +1,9 @@
 User.create! do |u|
-  u.email = 'admin@sac.com'
+  u.email = 'zachary.green.16@us.af.mil'
   u.password = 'passwordpassword'
   u.hq_role = true
   u.name = 'Admin'
-  u.office_symbol = 'HQ'
-  u.work_phone_number = 'xxx-xxxx'
+  u.office_symbol = ''
+  u.work_phone_number = ''
 end 
-User.create! do |u|
-  u.office_id = 1
-  u.email = 'user@sac.com'
-  u.password = 'passwordpassword'
-  u.name = 'User'
-  u.office_symbol = 'office_sym'
-  u.work_phone_number = 'xxx-xxxx'
-end
-User.create! do |u|
-  u.office_id = 1
-  u.email = 'sfs@sac.com'
-  u.password = 'passwordpassword'
-  u.installation = 'Barksdale'
-  u.wing = '2nd Bomb Wing'
-  u.grp = 'MSG'
-  u.unit = '2 SFS'
-  u.name = 'SFS RA'
-  u.office_symbol = '2 SFS/RA'
-  u.work_phone_number = 'xxx-xxxx'
-end
-User.create! do |u|
-  u.office_id = 1
-  u.email = 'msg_ra@sac.com'
-  u.password = 'passwordpassword'
-  u.installation = 'Barksdale'
-  u.wing = '2nd Bomb Wing'
-  u.grp = 'MSG'
-  u.name = 'MSG RA'
-  u.office_symbol = 'MSG/RA'
-  u.work_phone_number = 'xxx-xxxx'
-end
-User.create! do |u|
-  u.office_id = 1
-  u.email = 'fma@sac.com'
-  u.password = 'passwordpassword'
-  u.installation = 'Barksdale'
-  u.wing = '2nd Bomb Wing'
-  u.fma_role = true
-  u.name = 'FMA Office Tech'
-  u.office_symbol = '2 CPTS/FMA'
-  u.work_phone_number = 'xxx-xxxx'
-end
+
