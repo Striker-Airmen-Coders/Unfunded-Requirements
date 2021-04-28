e00cfd07cc38c8c63146d4e9fd983dc272c3d0f2
moldux.dartus <moldux.dartus@protonmail.com>
2021-04-28 14:08:35 -0500

    question text and type no longer optional; blank text was causing crash

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,10 +5,14 @@ class Question < ApplicationRecord
   has_many :answers
 
   validates :type,
+    presence: true,
     inclusion: {
       in: %w(text number date boolean),
       message: "%{value} is not a valid type"
     }
+
+  validates :text, presence: true
+
   validates :key,
     uniqueness: true,
     unless: -> { key.blank? }
